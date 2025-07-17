27a3c0c8847d7e84319f1d59626c5f332889c62f
noah <noa2006@naver.com>
2025-07-18 01:53:57 +0900

    상품조회, 상위상품 조회

---
+++ src/main/java/kr/hhplus/be/server/product/dto/PopularProductResponse.java
@@ -0,0 +1,24 @@
+package kr.hhplus.be.server.product.dto;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+
+import java.time.LocalDateTime;
+
+public class PopularProductResponse {
+    public record Details(
+            @Schema(description = "상품 ID", requiredMode = RequiredMode.REQUIRED)
+            long id,
+            @Schema(description = "상품 이름", requiredMode = RequiredMode.REQUIRED)
+            String name,
+            @Schema(description = "판매량", requiredMode = RequiredMode.REQUIRED)
+            int salesCount,
+            @Schema(description = "조회수", requiredMode = RequiredMode.REQUIRED)
+            int viewCount,
+            @Schema(description = "등록일", requiredMode = RequiredMode.REQUIRED, format = "date-time")
+            LocalDateTime regDt
+
+    ) {}
+
+}
---
+++ src/main/java/kr/hhplus/be/server/product/dto/ProductResponse.java
@@ -0,0 +1,23 @@
+package kr.hhplus.be.server.product.dto;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+
+public class ProductResponse {
+    public record Details(
+            @Schema(description = "상품 ID", requiredMode = RequiredMode.REQUIRED)
+            Long id,
+            @Schema(description = "상품 이름", requiredMode = RequiredMode.REQUIRED)
+            String name,
+            @Schema(description = "상품 가격", requiredMode = RequiredMode.REQUIRED)
+            int price,
+            @Schema(description = "총 수량", requiredMode = RequiredMode.REQUIRED)
+            int totalQuantity,
+            @Schema(description = "현재 재고", requiredMode = RequiredMode.REQUIRED)
+            int stockQuantity,
+            @Schema(description = "상품 상태", requiredMode = RequiredMode.REQUIRED)
+            String status
+    ) {}
+
+}
