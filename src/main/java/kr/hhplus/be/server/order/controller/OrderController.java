3b946a59531c41f88b1df8d1130ba16be3b6c003
noah <noa2006@naver.com>
2025-07-18 09:21:01 +0900

    주문/결제 수정

--- src/main/java/kr/hhplus/be/server/order/controller/OrderController.java
+++ src/main/java/kr/hhplus/be/server/order/controller/OrderController.java
@@ -1,21 +1,19 @@
 package kr.hhplus.be.server.order.controller;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import kr.hhplus.be.server.order.dto.OrderRequest;
 import kr.hhplus.be.server.order.dto.OrderResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping("/api/v1/users/{userId}/orders/{orderId}")
 public class OrderController{
 
-    @GetMapping("/{orderId}")
-    public ResponseEntity<OrderResponse.Detail> getOrder(@PathVariable Long orderId) {
+    @PostMapping
+    public ResponseEntity<OrderResponse.Detail> getOrder(
+            @PathVariable Long orderId,@PathVariable Long userId
+    ) {
         List<OrderResponse.OrderItem> items = List.of(
                 new OrderResponse.OrderItem(1L, 1L, 1, 29900, LocalDateTime.now()),
                 new OrderResponse.OrderItem(2L, 2L, 1, 29900, LocalDateTime.now())
@@ -24,7 +22,8 @@ public class OrderController{
                 1L, 52000, "CARD", "결제완료", LocalDateTime.now(), LocalDateTime.now()
         );
         OrderResponse.Detail response = new OrderResponse.Detail(
-                orderId, 1L, 1L, 59800, 7800, 52000, "결제완료", LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now(),
+                orderId, userId, 1L, 59800, 7800, 52000, "결제완료",
+                LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now(),
                 items, payment
         );
         return ResponseEntity.ok(response);
