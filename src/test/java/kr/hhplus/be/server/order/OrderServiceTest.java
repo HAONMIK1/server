6051892a6c64e671d4abcdc1004627ba96f3877f
noah <noa2006@naver.com>
2025-07-25 02:03:39 +0900

    주문 결제 테스트

---
+++ src/test/java/kr/hhplus/be/server/order/OrderServiceTest.java
@@ -0,0 +1,31 @@
+package kr.hhplus.be.server.order;
+
+import org.junit.jupiter.api.Test;
+
+
+class OrderServiceTest {
+
+    @Test
+    void 주문_성공() {
+
+    }
+    @Test
+    void 잔액_부족_실패() {
+
+    }
+    @Test
+    void 쿠폰_적용_주문_성공() {
+
+    }
+
+    @Test
+    void 재고_부족_주문_실패() {
+
+    }
+
+    @Test
+    void 잔액_부족_주문_실패() {
+
+    }
+
+}
