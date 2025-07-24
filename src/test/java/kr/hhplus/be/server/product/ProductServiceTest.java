7f2aab3ae30b497566f2f926dfa040327c3ec0c2
noah <noa2006@naver.com>
2025-07-25 00:30:27 +0900

    상품 목록 조회 / 상세 조회 테스트

---
+++ src/test/java/kr/hhplus/be/server/product/ProductServiceTest.java
@@ -0,0 +1,18 @@
+package kr.hhplus.be.server.product;
+
+import org.junit.jupiter.api.Test;
+
+
+class ProductServiceTest {
+
+    @Test
+    void 상품_목록_조회_성공() {
+
+    }
+
+    @Test
+    void 상품_상세_조회_성공() {
+
+    }
+
+}
