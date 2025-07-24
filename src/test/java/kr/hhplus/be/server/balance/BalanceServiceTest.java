09c03ffd0c3c70c87da964c255af27227c80563f
noah <noa2006@naver.com>
2025-07-24 23:55:55 +0900

    잔액 충전 사용 조회 테스트

---
+++ src/test/java/kr/hhplus/be/server/balance/BalanceServiceTest.java
@@ -0,0 +1,55 @@
+package kr.hhplus.be.server.balance;
+
+import org.junit.jupiter.api.Test;
+
+
+class BalanceServiceTest {
+
+    @Test
+    void 잔액_충전_성공() {
+
+    }
+    @Test
+    void 잔액_최소충전_성공() {
+
+    }
+    @Test
+    void 잔액_최대충전_성공() {
+
+    }
+    @Test
+    void 잔액_최소충전경계값_성공() {
+
+    }
+    @Test
+    void 잔액_최대충전경계값_성공() {
+
+    }
+    @Test
+    void 최대잔고_경계값_성공() {
+
+    }
+
+    @Test
+    void 잔액_사용_성공() {
+
+    }
+
+    @Test
+    void 잔액_부족_실패() {
+
+    }
+
+    @Test
+    void 음수_금액_사용_실패() {
+
+    }
+    @Test
+    void 사용_이력_저장_성공() {
+
+    }
+    @Test
+    void 충전_이력_저장_성공() {
+
+    }
+}
