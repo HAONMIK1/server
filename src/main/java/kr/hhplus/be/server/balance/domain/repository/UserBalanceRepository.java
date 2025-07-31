446b9ffcfccb2f273c9a9924fcd051fa39d3671f
noah <noa2006@naver.com>
2025-08-01 03:23:33 +0900

    포인트 infrastructure Layer 구현 및 테스트 2

--- src/main/java/kr/hhplus/be/server/balance/domain/repository/UserBalanceRepository.java
+++ src/main/java/kr/hhplus/be/server/balance/domain/repository/UserBalanceRepository.java
@@ -1,8 +1,8 @@
 package kr.hhplus.be.server.balance.domain.repository;
 
 import kr.hhplus.be.server.balance.domain.entity.UserBalanceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserBalanceRepository extends JpaRepository<UserBalanceEntity, Long> {
-    UserBalanceEntity  findByUserId(Long userId);
+public interface UserBalanceRepository {
+    UserBalanceEntity findByUserId(Long userId);
+    UserBalanceEntity save(UserBalanceEntity userBalance);
 }
