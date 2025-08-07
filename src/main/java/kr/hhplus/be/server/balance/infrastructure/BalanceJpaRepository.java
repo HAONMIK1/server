359f1e4db4586e4f029c28756f10d30867beb1bd
noah <noa2006@naver.com>
2025-08-08 02:40:03 +0900

    포인트 트랜젝션 처리 및 비관적 락 처리

--- src/main/java/kr/hhplus/be/server/balance/infrastructure/BalanceJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/balance/infrastructure/BalanceJpaRepository.java
@@ -1,7 +1,9 @@
 package kr.hhplus.be.server.balance.infrastructure;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.balance.domain.entity.UserBalanceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,8 +12,9 @@ import java.util.Optional;
 
 @Repository
 public interface BalanceJpaRepository extends JpaRepository<UserBalanceEntity, Long> {
-    
-    @Query("SELECT ub FROM UserBalanceEntity ub WHERE ub.userId = :userId")
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT ub FROM UserBalanceEntity ub WHERE ub.userId = :userId ")
     Optional<UserBalanceEntity> findByUserId(@Param("userId") Long userId);
     
 }
